6b7c117d50e3bf0c5ffcc63e3ad6c3f35ab4abe9
rvnagmode <101249215+rvnagmode@users.noreply.github.com>
2022-03-17 10:36:15 +0530

    abstract

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Day4
+﻿using Day4;
+namespace Day4
 {
     public class Testing
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("area of circle");
+            Circle c = new Circle(12);
+            c.CalculateArea();
+            Console.WriteLine(c);
 
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("area of rectangle");
+            Rectangle r = new Rectangle(10,20);
+            r.CalculateArea();
+            Console.WriteLine(r);
         }
     }
 }
---
+++ Day4/Shape.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public abstract class Shape
+    {
+        abstract public void CalculateArea();
+    }
+    public class Circle : Shape
+    {
+        private int r;
+        private double area;
+
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+        public override void CalculateArea()
+        {
+            area = 3.14 * r * r;
+        }
+        public override string ToString()
+        {
+            return "area of circle = " + area;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private int l, b;
+        private double area;
+        public Rectangle(int l,int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public override void CalculateArea()
+        {
+            area = l * b;
+        }
+        public override string ToString()
+        {
+            return "area of rectangle = " + area;
+        }
+    }
+}
